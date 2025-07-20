a22afb963186055dc91823b962d7168812265e8c
oluoko <brianotieno586@gmail.com>
2025-07-20 22:31:55 +0300

    Blogger: Setting up the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="login" element={<div>Login Page</div>} />
+        <Route path="register" element={<div>Register Page</div>} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
